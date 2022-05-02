9a8e00915e20d3d4c0dadd38cda902241d610415
amarnathp <amarnathp.official@gmail.com>
2022-05-02 19:06:12 +0530

    test

--- modules/o2o/ionjs/libs/manifestRenderer.js
+++ modules/o2o/ionjs/libs/manifestRenderer.js
@@ -19,16 +19,28 @@ function render(m) {
 function styleRenderer(m) {
   var style = document.createElement('style');
   var rs;
-  if (!!m.backgroundColor) style.innerHTML = `
-    * { background: ${m.backgroundColor};}
+  if (!!m.backgroundColor) style.innerHTML += `
+    body { background: ${m.backgroundColor};}
   `;
-  document.head.appendChild(style);
   if (!!m.themeColor) {
     var color = document.createElement('meta');
     color.name = 'theme-color';
     color.content = m.themeColor;
     document.head.appendChild(color);
   }
+  if (!!m.color) style.innerHTML += `
+    body { color: ${m.color};}
+  `;
+  if (!!m.padding) style.innerHTML += `
+    body { padding: ${m.padding};}
+  `;
+  if (!!m.margin) style.innerHTML += `
+    body { margin: ${m.margin};}
+  `;
+  if (!!m.color) style.innerHTML += `
+    body { color: ${m.color};}
+  `;
+  document.head.appendChild(style);
 }
 
 export default manifestRenderer;
